30430f8a963e08050e5dadbc1cfc99a01b6a0d99
Christopher Esparza <christopher.esparza@hotmail.com>
2022-04-02 15:32:48 -0700

    No terminado
    splash

--- Secure Car/ViewController.m
+++ Secure Car/ViewController.m
@@ -18,5 +18,21 @@
     // Do any additional setup after loading the view.
 }
 
+- (void)viewDidAppear:(BOOL)animated{
+    self.imvSplash.alpha = 0;
+    
+    self.imvSplash.frame =
+    CGRectMake(self.imvSplash.frame.origin.x, -200, self.imvSplash.frame.size.width, self.imvSplash.frame.size.height);
+    
+    
+    [UIView animateWithDuration:4 animations:^{
+        self.imvSplash.alpha = 1;
+        self.imvSplash.frame =
+        CGRectMake(self.imvSplash.frame.origin.x, self.view.frame.size.height/2 - self.imvSplash.frame.size.height/2, self.imvSplash.frame.size.width, self.imvSplash.frame.size.height);
+    } completion:^(BOOL finished) {
+        
+    }];
+}
+
 
 @end
